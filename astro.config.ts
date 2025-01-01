947b65255d6e06a72cf990b41f7d4c9f11e1e592
rizky <rizky05@gmail.com>
2025-01-01 15:45:55 +0700

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,16 @@
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import react from '@astrojs/react';
+const is_win = process.platform === 'win32';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
   compressHTML: true,
+  image: is_win ? {
+    service: passthroughImageService()
+  } : undefined,
   integrations: [tailwind(), compress(), react()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,13 +2,14 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 export const works = defineCollection({
-  loader: glob({ base: './works', pattern: '**/*.yaml' }),
+  loader: glob({ base: 'works', pattern: '**/*.yaml' }),
   schema: () =>
     z.object({
       name: z.string(),
       tags: z.array(z.string()),
       desc: z.string(),
       images: z.array(z.string()),
+      size: z.string(),
       est_hours: z.number(),
       est_days: z.number(),
       services_used: z.array(z.string()),
