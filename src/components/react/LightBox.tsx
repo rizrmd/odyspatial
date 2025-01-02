5e84cfbd458ea3a7e46c67fc4e67e988584817bd
Rizky <rizky@avolut.com>
2025-01-02 08:41:19 +0700

    fix ligtbox

--- src/components/react/LightBox.tsx
+++ src/components/react/LightBox.tsx
@@ -22,7 +22,8 @@ export default function LightBox({
     pos: 0,
     show_nav: false,
     img: image,
-    show_wobble: true,
+    ival: null as any,
+    div: null as unknown as HTMLDivElement,
     loading_text: [
       'Rendering Elegance',
       'Crafting Spaces',
@@ -57,21 +58,6 @@ export default function LightBox({
           local.img = image;
           local.pos = images?.findIndex((e) => e.src === image.src) || 0;
 
-          if (typeof w.touch_anim_preview !== 'number') {
-            w.touch_anim_preview = localStorage.getItem('touch_anim_preview')
-              ? parseInt(localStorage.getItem('touch_anim_preview') || '0')
-              : 0;
-          }
-          if (w.touch_anim_preview < 5) {
-            w.touch_anim_preview++;
-            localStorage.setItem('touch_anim_preview', w.touch_anim_preview.toString());
-          }
-
-          setTimeout(() => {
-            local.show_wobble = false;
-            render();
-          }, 2000);
-
           const cal = document.querySelector('cal-floating-button') as HTMLDivElement;
           if (cal) {
             cal.style.display = 'none';
@@ -84,6 +70,9 @@ export default function LightBox({
       {local.open &&
         createPortal(
           <div
+            ref={(el) => {
+              if (el) local.div = el;
+            }}
             className={cx(
               css`
                 position: fixed;
@@ -118,6 +107,7 @@ export default function LightBox({
             onClick={(e) => {
               e.preventDefault();
 
+              clearInterval(local.ival);
               local.open = false;
               const cal = document.querySelector('cal-floating-button') as HTMLDivElement;
               if (cal) {
@@ -125,38 +115,45 @@ export default function LightBox({
               }
               render();
             }}
+            onPointerMove={() => {
+              clearInterval(local.ival);
+            }}
           >
             <img
               onLoad={() => {
                 local.loading = false;
                 render();
+
+                if (is_mobile) {
+                  local.div.scrollLeft = 0;
+                  clearInterval(local.ival);
+                  setTimeout(() => {
+                    let last = -1;
+                    local.ival = setInterval(() => {
+                      if (local.div.scrollLeft === last) {
+                        clearInterval(local.ival);
+                      } else {
+                        last = local.div.scrollLeft;
+                        local.div.scrollLeft += 0.5;
+                      }
+                      render();
+                    }, 10);
+                  }, 500);
+                }
               }}
               className={cx('motion-preset-slide-up-sm select-none')}
               src={`/_image?href=${local.img.src}&w=${local.img.width}&f=webp`}
             />
             {local.loading && (
-              <div className="motion-preset-fade fixed inset-0 flex items-center justify-center bg-primary-950 bg-opacity-50 text-white">
+              <div
+                className={cx(
+                  'motion-preset-fade fixed inset-0 flex items-center justify-center bg-primary-950 bg-opacity-50 text-white'
+                )}
+              >
                 {local.loading_text[Math.floor(Math.random() * local.loading_text.length)]}
               </div>
             )}
-            {w.touch_anim_preview <= 3 && local.show_wobble && is_mobile && (
-              <div className="motion-preset-fade pointer-events-none fixed inset-0 flex items-center justify-center bg-opacity-50 text-white">
-                <div className="flex flex-1 items-center justify-center bg-primary-950 bg-opacity-50 py-[30px]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={'50'}
-                    viewBox="0 0 32 32"
-                    className="motion-preset-wobble-lg motion-duration-1000 motion-loop-infinite"
-                  >
-                    <path fill="currentColor" d="M20 24h-2a5 5 0 0 1-10 0H6a7 7 0 0 0 14 0Z" />
-                    <path
-                      fill="currentColor"
-                      d="M28 14V8a7.008 7.008 0 0 0-7-7h-5a6.146 6.146 0 0 0-4.106 1.566l-8.01 7.308a2.999 2.999 0 0 0 3.88 4.55l.001.001L10 12.895V24a3 3 0 0 0 6 0v-5.184a2.939 2.939 0 0 0 3.53-1.217A2.963 2.963 0 0 0 21 18a2.994 2.994 0 0 0 2.53-1.401A2.963 2.963 0 0 0 25 17a3.003 3.003 0 0 0 3-3Zm-2 0a1 1 0 0 1-2 0v-1h-2v2a1 1 0 0 1-2 0v-2h-2v3a1 1 0 0 1-2 0v-3h-2v11a1 1 0 0 1-2 0V9.104l-5.4 3.697a1 1 0 0 1-1.308-1.505l7.95-7.251A4.148 4.148 0 0 1 16 3h5a5.006 5.006 0 0 1 5 5Z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            )}
+
             {local.show_nav && (
               <>
                 <div
